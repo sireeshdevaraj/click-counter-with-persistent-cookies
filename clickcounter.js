e84bb26a1e13d57d571f26fa155baebb9581d809
『KūHaku』 <kongarasireeshdevaraj@gmail.com>
2022-05-25 16:36:02 +0530

    simple additions

--- clickcounter.js
+++ clickcounter.js
@@ -2,10 +2,10 @@ let c=0;
 document.addEventListener('click',()=> {
      c=c+1;
      document.getElementById("clicks").innerHTML=c;
-     document.getElementById("clicks").style.fontSize=Math.random()*500
+     document.getElementById("clicks").style.fontSize=Math.random()*500 //Just for fun,random font for every click xD
      let user = getCookie("username");
      if (user != ""){
-      try{
+      try{ //try to split the cookie,if the cookie is not present just keep on going with the current clicks and store the final value to highscore
           var a =document.cookie.split(';')[1].slice(15)
           var b= parseInt(a)
       if (b<c){
@@ -19,7 +19,7 @@ document.addEventListener('click',()=> {
         document.cookie=`Highest Score = ${c};`;
      }
      if (user=""){
-        document.cookie=`Highest Score = ${c};`
+        document.cookie=`Highest Score = ${c};` //adding the score 
      }
      
      
